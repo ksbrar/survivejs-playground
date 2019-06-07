699fc9aa3886aabae5d54698d88a319750593085
ksbrar <kabir@brar.xyz>
2019-06-07 14:32:33 -0700

    Temp.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require("webpack");
+
 require('dotenv').config();
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
@@ -7,11 +9,13 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: "Webpack demo",
         }),
+        new webpack.HotModuleReplacementPlugin(),
     ],
 
     devServer: {
         stats: "errors-only",
         overlay: true,
+        hotOnly: true,
 
         host: process.env.host,
         port: process.env.ENV_PORT,
